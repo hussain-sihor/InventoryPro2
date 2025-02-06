2b7b71a2c6f4c30e546904096f63640320a0dc73
Hussain sihor <sihorwalahussain@gmail.com>
2025-02-06 17:30:54 +0530

    final deployment

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an Axios instance with a base URL
 // http://localhost:5000/api/
 const axiosInstance = axios.create({
-    baseURL: 'https://inventorybackend-gray.vercel.app/',
+    baseURL: 'https://inventorybackend-gray.vercel.app/api/',
 });
 
 
